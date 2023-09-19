6adf5cde0c2a073e0727d3ae5e3067e7fa1bc543
Mecanight <cassianoprediger@gmail.com>
2023-09-19 02:33:45 -0300

    Aula-12-09-2023-(correcoes-complemento-video-revisao)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,17 +4,23 @@ import { AppService } from './app.service';
 import { ProfileController } from './controllers/profile.controller';
 import { ProfileService } from './services/profile.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './controllers/user.controller';
+import { UserService } from './services/user.service';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3306,
     username: 'root',
     password: '',
-    database: 'prm_2023'
-  })],
-  controllers: [AppController, ProfileController],
-  providers: [AppService, ProfileService],
+    database: 'prm_2023',
+    entities: [User]
+  }),
+  TypeOrmModule.forFeature([User])
+],
+  controllers: [AppController, ProfileController, UserController],
+  providers: [AppService, ProfileService, UserService],
 })
 export class AppModule {}
